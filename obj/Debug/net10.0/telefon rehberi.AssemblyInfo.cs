1511e5daf9b0e0414660bf68583b1eb3f8dd66fb
hakankaradeniz44 <hakanlcwaikikimobile@gmail.com.com>
2025-12-02 16:33:42 +0300

    telefonrehberi

--- obj/Debug/net10.0/telefon rehberi.AssemblyInfo.cs
+++ obj/Debug/net10.0/telefon rehberi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("telefon rehberi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad22bb9c0fc9bd084e8a69f4606bc03f2a3e40c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("telefon rehberi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("telefon rehberi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
